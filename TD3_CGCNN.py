3ff6902fb86a3f2f616a33d7606e2b84a474836c
Elena Trukhan <105585777+ElenaTrukhan@users.noreply.github.com>
2025-01-30 13:36:16 +0300

    Update TD3_CGCNN.py

--- TD3_CGCNN.py
+++ TD3_CGCNN.py
@@ -14,6 +14,7 @@ parser = argparse.ArgumentParser(description='TD3')
 
 #Environment parameters 
 parser.add_argument('--structures_file', type=str, help='Path to the file with input structures')
+parser.add_argument('--interval', type=eval, default=None, help='The range of structures from structures_file that will be used for training')
 parser.add_argument('--eps', type=float, default=0.1, help='Force threshold, eV/A')
 parser.add_argument('--reward_func', type=str, default='step', help='Reward function type: force | log_force | step | hybrid')
 parser.add_argument('--r_weights', type=eval, default=[1, 0, 0.5], help='Weights for the hybrid reward function')
@@ -67,7 +68,7 @@ parser.add_argument('--n_l_c', type=eval, default=[30,50, 120,20,60], help = 'Hi
 
 args = parser.parse_args()
 
-s_lib, calcs = get_sturct_lib_and_calcs_gen(args.structures_file)
+s_lib, calcs = get_sturct_lib_and_calcs_gen(args.structures_file, interval = args.interval)
 
 env_kwards = {"input_struct_lib": s_lib, "convert_to_graph_func": to_graph, 
                 "calculator_lib": calcs, "r0":args.r0 , "eps" : args.eps, 
